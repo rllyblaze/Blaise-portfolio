710c289a08c0fe20151d5eb497e601111a5bdef6
rllyblaze <blaise.adabiokou6@gmail.com>
2025-02-12 18:12:25 -0500

    Finished portolio test

---
+++ dark-mode.js
@@ -0,0 +1,31 @@
+const darkMode = () => {
+    const themeToggleBtns = document.querySelectorAll('#theme-toggle');
+    
+    //State
+    const theme = localStorage.getItem('theme');
+    
+    //On Mount
+    theme && document.body.classList.add(theme);
+    
+    //Handlers
+    const handleThemeToggle = () => {
+            document.body.classList.toggle('light-mode');
+            if (document.body.classList.contains('light-mode')) {
+                localStorage.setItem('theme', 'light-mode');
+            } else {
+                localStorage.removeItem('theme');
+                document.body.removeAttribute('class');
+            }
+            
+    };
+    
+    
+    //Events
+    themeToggleBtns.forEach(btn => 
+            btn.addEventListener('click', handleThemeToggle)
+        );
+    };
+    
+    export default darkMode;
+
+    darkMode();
